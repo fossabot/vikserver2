00d382b66a8f9ba5e5bc60fea0d5901c029c83f6
MrViK <mrvikxd@gmail.com>
2017-07-31 01:21:39 +0200

    Añadimos cosas para debug

--- .travis/after_success.sh
+++ .travis/after_success.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+echo "Creando directorio para FTP"
 mkdir .travis/ftp
+echo "Montando FTP con fuse"
 sudo curlftpfs -o allow_other,user=$FTP_USER:$FTP_PASSWORD ftp://victor.zona.digital .travis/ftp
-(cp -urv * .travis/ftp/public_html && cp -urv * .travis/ftp/public_shtml && sudo umount .travis/ftp && exit 0) || (echo "Ha habido un fallo en el deploy" && exit 1)
+echo "Copiando los archivos al FTP"
+cp -urv * .travis/ftp/public_html
+cp -urv * .travis/ftp/public_shtml
+sudo umount .travis/ftp 
+exit 0

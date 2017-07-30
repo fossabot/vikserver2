701ab695ba66d015c97976394e3454c6d638b503
MrViK <mrvikxd@gmail.com>
2017-07-30 16:54:31 +0200

    El deploy debería hacerse en otra ubicación

--- .travis/after_success.sh
+++ .travis/after_success.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 for i in $(ls); do
-	curl --ftp-create-dirs -T "$i" -u $FTP_USER:$FTP_PASSWORD ftp://victor.zona.digital/public_shtml/
+	#curl --ftp-create-dirs -T "$i" -u $FTP_USER:$FTP_PASSWORD ftp://victor.zona.digital/public_shtml/
+	curl --ftp-create-dirs -T "$i" -u $FTP_USER:$FTP_PASSWORD ftp://victor.zona.digital/public_html/
 done

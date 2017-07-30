83a9cc57a63a7b47c98c3c2eaa00a8f5c3bc5034
MrViK <mrvikxd@gmail.com>
2017-07-30 17:31:42 +0200

    Ahora creamos la carpeta de montura desde travis

--- .travis/after_success.sh
+++ .travis/after_success.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 for i in $(ls); do
+	mkdir .travis/ftp
 	curlftpfs -o user=$FTP_USER:$FTP_PASSWORD ftp://victor.zona.digital .travis/ftp
 	cp -ur * .travis/ftp/public_html
 	cp -ur * .travis/ftp/public_shtml

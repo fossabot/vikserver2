f20de9907233f60bb12d69f245a4e0b5926815d0
MrViK <mrvikxd@gmail.com>
2017-07-30 23:48:46 +0200

    Debemos permitir al usuario copiar archivos

--- .travis/after_success.sh
+++ .travis/after_success.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 mkdir .travis/ftp
-sudo curlftpfs -o user=$FTP_USER:$FTP_PASSWORD ftp://victor.zona.digital .travis/ftp
+sudo curlftpfs -o allow_other,user=$FTP_USER:$FTP_PASSWORD ftp://victor.zona.digital .travis/ftp
 (cp -ur * .travis/ftp/public_html && cp -ur * .travis/ftp/public_shtml && sudo umount .travis/ftp && exit 0) || (echo "Ha habido un fallo en el deploy" && exit 1)

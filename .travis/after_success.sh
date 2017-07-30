20a555a811e93e837b42a7ee8c678fa6cfd35215
MrViK <mrvikxd@gmail.com>
2017-07-30 23:04:29 +0200

    No se desmontaba correctamente

--- .travis/after_success.sh
+++ .travis/after_success.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 mkdir .travis/ftp
 curlftpfs -o user=$FTP_USER:$FTP_PASSWORD ftp://victor.zona.digital .travis/ftp
-cp -ur * .travis/ftp/public_html
-cp -ur * .travis/ftp/public_shtml
-fusermount -u .travis/ftp
+(cp -ur * .travis/ftp/public_html && cp -ur * .travis/ftp/public_shtml && sudo umount .travis/ftp && exit 0) || (echo "Ha habido un fallo en el deploy" && exit 1)

9520db002c5041cb4fe95884ef844ae6b8c3791b
MrViK <mrvikxd@gmail.com>
2017-07-31 19:57:36 +0200

    Añadidos más mensajes

--- .travis/after_success.sh
+++ .travis/after_success.sh
@@ -6,5 +6,7 @@ sudo curlftpfs -o allow_other,user=$FTP_USER:$FTP_PASSWORD ftp://victor.zona.dig
 echo "Copiando los archivos al FTP"
 cp -urv * .travis/ftp/public_html
 #cp -urv * .travis/ftp/public_shtml # Creo que no hará falta copiar a este directorio
-sudo umount .travis/ftp 
+echo "Desmontando FTP"
+sudo umount .travis/ftp
+echo "Copia terminada"
 exit 0

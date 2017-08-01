3d07b5164814fd330a7bcfdb456ddd4cb6cfb824
MrViK <mrvikxd@gmail.com>
2017-08-01 09:10:12 +0200

    Cambiamos cp por rsync

--- .travis/after_success.sh
+++ .travis/after_success.sh
@@ -6,8 +6,8 @@ mkdir .travis/ftp
 echo "Montando FTP con fuse"
 sudo curlftpfs -o allow_other,user=$FTP_USER:$FTP_PASSWORD ftp://victor.zona.digital .travis/ftp
 echo "Copiando los archivos al FTP"
-cp -urv * .travis/ftp/public_html
-#cp -urv * .travis/ftp/public_shtml # Creo que no hará falta copiar a este directorio
+rsync --info=progress2 * .travis/ftp/public_html
+#cp -urv * .travis/ftp/public_html
 echo "Desmontando FTP"
 sudo umount .travis/ftp
 echo "Copia terminada"

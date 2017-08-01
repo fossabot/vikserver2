2aa4651db3e1d7a0b07ed6f3b6e0d6db1dcea104
MrViK <mrvikxd@gmail.com>
2017-08-01 19:45:53 +0200

    Ahora se crea un enlace simbólico a las fuentes de FA

--- .travis/after_success.sh
+++ .travis/after_success.sh
@@ -5,6 +5,8 @@ echo "Limpiando elementos inecesarios"
 rm -rf lib/fontAwesome/src lib/fontAwesome/scss lib/fontAwesome/less
 rm -rf lib/pace/docs lib/pace/templates
 rm -rf lib/typedjs/docs lib/typedjs/src
+echo "Limpiando enlaces simbólicos"
+unlink fonts
 echo "Creando directorio para FTP"
 mkdir .travis/ftp
 echo "Montando FTP con fuse"
@@ -12,6 +14,8 @@ sudo curlftpfs -o allow_other,user=$FTP_USER:$FTP_PASSWORD ftp://victor.zona.dig
 echo "Copiando los archivos al FTP"
 #rsync --info=progress2 * -v -r .travis/ftp/public_html
 cp -rv * .travis/ftp/public_html
+echo "Creando nuevos enlaces simbólicos"
+ln -sv fonts/fontAwesome/fonts .travis/FTP/fonts
 echo "Desmontando FTP"
 sudo umount .travis/ftp
 echo "Copia terminada"

970ef259c4403e4572ca8e0b1e22ac93d48e46cc
MrViK <mrvikxd@gmail.com>
2017-08-01 15:19:23 +0200

    Volvemos a usar 'cp' para copiar

--- .travis/after_success.sh
+++ .travis/after_success.sh
@@ -6,8 +6,8 @@ mkdir .travis/ftp
 echo "Montando FTP con fuse"
 sudo curlftpfs -o allow_other,user=$FTP_USER:$FTP_PASSWORD ftp://victor.zona.digital .travis/ftp
 echo "Copiando los archivos al FTP"
-rsync --info=progress2 * -v -r .travis/ftp/public_html
-#cp -urv * .travis/ftp/public_html
+#rsync --info=progress2 * -v -r .travis/ftp/public_html
+cp -rv * .travis/ftp/public_html
 echo "Desmontando FTP"
 sudo umount .travis/ftp
 echo "Copia terminada"
